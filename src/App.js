ccd25b3c8291d7acd44feda29ecb2988309095ba
arifbilir <arifbilirde@gmail.com>
2023-03-03 23:21:15 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 
 import './App.scss';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Main from './components/Main/Main';
 
 const App = () => {
   return(
-    <div className='App'>
-      <h1>Welcome React</h1>
-      <p>
-        loreememdaed daedea def eafea faef efea fea faa
-      </p>
+    <div className="App">
+      <Navbar/>
+      <Header/>
+      <Main/>
     </div>
   )
 }
 
 export default App;
+
+
